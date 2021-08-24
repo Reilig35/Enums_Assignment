3377b97c1c838db2f614ae47b9e98f8bc28079e2
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-24 19:32:57 +0100

    Add project files.

---
+++ Enums_Assignment/DaysOfWeek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enums_Assignment
+{
+    public class DaysOfWeek
+    {
+    }
+
+    public enum Days_Of_The_Week
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ Enums_Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enums_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type what day it is today:\n");
+            var userInput = Console.ReadLine();
+            try
+            {
+                var currentDay = Enum.Parse(typeof(Days_Of_The_Week), userInput);
+                Console.WriteLine($"\n\nToday is {currentDay}");
+            }
+
+            catch (System.ArgumentException ex)
+            {
+                Console.WriteLine("\n\nPlease type a day of the week");
+            }
+            finally
+            {
+                Console.WriteLine("\n\nThank you for using my programme");
+            }
+        }
+    }
+}
+            
+               
+                
+
+
